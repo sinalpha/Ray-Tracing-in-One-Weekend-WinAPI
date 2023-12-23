edfccf3dd4ce6b4d391aff4bded0bc03f517db94
ssinalpha <hanhdhx65@gmail.com>
2023-12-24 02:20:36 +0900

    원 교차 검사 개선

--- RTIWK-win/main.cpp
+++ RTIWK-win/main.cpp
@@ -26,12 +26,12 @@ point3 pixel00_loc;
 
 double hit_sphere(const point3& center, double radius, const ray& r) {
 	vec3 oc = r.origin() - center;
-	double a = dot(r.direction(), r.direction());
-	double b = 2.0 * dot(oc, r.direction());
-	double c = dot(oc, oc) - radius * radius;
-	double discriminant = b * b - 4 * a * c;
+	double a = r.direction().length_squared();
+	double half_b = dot(oc, r.direction());
+	double c = oc.length_squared() - radius * radius;
+	double discriminant = half_b * half_b - a * c;
 
-	return discriminant < 0 ? -1.0 : (-b - sqrt(discriminant)) / (2.0 * a);
+	return discriminant < 0 ? -1.0 : (-half_b - sqrt(discriminant)) / a;
 }
 
 color ray_color(const ray& r) {
