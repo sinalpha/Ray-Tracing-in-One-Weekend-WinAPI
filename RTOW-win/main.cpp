fa5acd9f2c292fecdb61aae7c6203ad68aa36832
ssinalpha <hanhdhx65@gmail.com>
2024-01-24 23:37:39 +0900

    totla internal reflection 구현

--- RTOW-win/main.cpp
+++ RTOW-win/main.cpp
@@ -21,7 +21,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance
 	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
 	auto material_center = make_shared<dielectric>(1.5);
 	auto material_left = make_shared<dielectric>(1.5);
-	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
 	//world
 	world->add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
